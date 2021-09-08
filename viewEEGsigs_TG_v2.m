b2ef10113ce55bf3a65431248574e00374773f8c
AamirAbbasi <aamirabbasi89@gmail.com>
2021-09-08 14:54:48 -0700

    Now data will be saved for individual patient with correct identifier.

--- viewEEGsigs_TG_v2.m
+++ viewEEGsigs_TG_v2.m
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-%% Get modulation depth of PSTHs from early to late trials
-%  Author - Aamir Abbasi
+%% EEG Analysis. 
+%  Authors - Aamir Abbasi & Tanuj Gulati
 %  -----------------------------------------------------------------------
-%% Modulation depth of PSTHs of M1 direct units
+%% Run this script to identify sleep epochs in human EEG data.
 clear;clc;close all;
 disp('running');
 rootpath = '/Volumes/gulatitlab-eegdata/New_patient_data/EEG_data/'; % Change this path based on the location of the files in your computer
@@ -12,7 +11,6 @@ eegfiles =  { 'xxxxxx_14acb982-496f-4c83-9940-28c90449057c'...
     ,'xxxxxx_c9c68c96-575e-49c0-9563-6222e4a8144a'...
     ,'xxxxxx_fa652153-9360-44ff-8376-3e8486fc26e2'};
 step = 20;
-chan = [1 ];
 temp=[];
 for i=1:length(eegfiles)
     disp([rootpath,eegfiles{i},'.mat']);
@@ -22,7 +20,7 @@ for i=1:length(eegfiles)
     eegsigs = ALLEEG.data;
     % creating differential signal like xltech
     temp=eegsigs;
-    temp=temp([1:22],:);
+    temp=temp(1:22,:);
     orders=[1,2,3,4,12,13,14,15,9,3,7,11,18,14,1,6,7,8,12,17,18,19; 2,3,4,5,13,14,15,16,3,7,11,18,14,20,6,7,8,5,17,18,19,16]';
     temp1=temp(orders(:,1),:);
     temp2=temp(orders(:,2),:);
@@ -42,49 +40,4 @@ for i=1:length(eegfiles)
     save([rootpath,'DATA_w_SleepEpochsID_',eegfiles{i},'.mat'],'sleepepochs','epochs','eegsigs','eegraw','fs','time')
 end
 
-=======
-%% Get modulation depth of PSTHs from early to late trials
-%  Author - Aamir Abbasi
-%  -----------------------------------------------------------------------
-%% Modulation depth of PSTHs of M1 direct units
-clear;clc;close all;
-disp('running');
-rootpath = '/Volumes/gulatitlab-eegdata/New_patient_data/EEG_data/'; % Change this path based on the location of the files in your computer
-eegfiles =  { 'xxxxxx_14acb982-496f-4c83-9940-28c90449057c'...
-    ,'xxxxxx_c2fbb91a-481e-407a-b1ad-81266f8691e5'...
-    ,'xxxxxx_c6f109c9-6345-4cb3-855d-48e186fd27c9'...
-    ,'xxxxxx_c9c68c96-575e-49c0-9563-6222e4a8144a'...
-    ,'xxxxxx_fa652153-9360-44ff-8376-3e8486fc26e2'};
-step = 20;
-chan = [1 ];
-temp=[];
-for i=1:length(eegfiles)
-    disp([rootpath,eegfiles{i},'.mat']);
-    load([rootpath,eegfiles{i},'.mat']);
-    fs = ALLEEG.srate; % sampling rate
-    time = ALLEEG.times;
-    eegsigs = ALLEEG.data;
-    % creating differential signal like xltech
-    temp=eegsigs;
-    temp=temp([1:22],:);
-    orders=[1,2,3,4,12,13,14,15,9,3,7,11,18,14,1,6,7,8,12,17,18,19; 2,3,4,5,13,14,15,16,3,7,11,18,14,20,6,7,8,5,17,18,19,16]';
-    temp1=temp(orders(:,1),:);
-    temp2=temp(orders(:,2),:);
-    temp4=temp1(:,:)-temp2(:,:);
-    eegsigs=temp4;
-    eegraw=temp;
-    %finished making differential signal
-    epochs = [];
-    bfr = 1:step:floor(length(eegsigs)/fs);
-    title1=eegfiles{i};
-    for j = bfr
-        if j ~= bfr(end)
-            epochs = cat(3,epochs,eegsigs(:,round(j*fs):round((j+step)*fs)));
-        end
-    end
-    sleepepochs = fn_visualizeEpochs_TG_v2(epochs,[], title1);
-    save([rootpath,'DATA_w_SleepEpochsID.mat'],'sleepepochs','epochs','eegsigs','eegraw','fs','time')
-end
-
->>>>>>> 41b072826980b71cf10409a99e3b0a4392504288
 %%
